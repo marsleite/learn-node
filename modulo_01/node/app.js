4967a32954154849d0d8a0e1d75040931249b989
Marcelo <marsleite@gmail.com>
2022-01-01 15:25:15 -0300

    criando endpoits

--- modulo_01/node/app.js
+++ modulo_01/node/app.js
@@ -1,8 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  req.setHeader('Content-type', text/html);
+  const url = req.url;
+  if (url === '/') {
+    res.write('<h1>Página principal</h1>');
+  } else if (url === '/contato') {
+    res.write('<h1>Página de contato</h1>');
+  } else {
+    res.write('<h1>Página não encontrada</h1>');
+  }
+  res.setHeader('Content-type', text/html);
   res.write('<h1>Hello World</h1>');
   res.end();
 });
