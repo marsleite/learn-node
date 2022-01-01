03182ab1e5ebac9303c8bb540bf82b22af6d3e30
Marcelo <marsleite@gmail.com>
2022-01-01 15:29:12 -0300

    retornos

--- modulo_01/node/app.js
+++ modulo_01/node/app.js
@@ -4,10 +4,13 @@ const server = http.createServer((req, res) => {
   const url = req.url;
   if (url === '/') {
     res.write('<h1>Página principal</h1>');
+    return res.end();
   } else if (url === '/contato') {
     res.write('<h1>Página de contato</h1>');
+    return res.end();
   } else {
     res.write('<h1>Página não encontrada</h1>');
+    return res.end();
   }
   res.setHeader('Content-type', text/html);
   res.write('<h1>Hello World</h1>');
