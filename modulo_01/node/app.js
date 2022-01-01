24e37d95fa96de2cafef973f69b1ae2e042d32bb
Marcelo <marsleite@gmail.com>
2022-01-01 15:04:44 -0300

    continuando a aula de node

--- modulo_01/node/app.js
+++ modulo_01/node/app.js
@@ -2,6 +2,9 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req);
+  req.setHeader('Content-type', text/html);
+  res.write('<h1>Hello World</h1>');
+  res.end();
 });
 
 server.listen(3000, () => {
